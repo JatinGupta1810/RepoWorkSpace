ca089ee8a16201a19e0ac8247d67a6ffc120e9f1
JatinGupta1810 <jatingupta3556@gmail.com>
2022-10-21 14:33:24 +0530

    deleted all files

--- Hi.java
+++
@@ -1,9 +0,0 @@
-
-public class PrintHello {
-
-	public static void main(String[] args) {
-
-		System.out.println("Hi to All....");
-	}
-
-}
--- PrintHello.java
+++
@@ -1,9 +0,0 @@
-
-public class PrintHello {
-
-	public static void main(String[] args) {
-
-		System.out.println("HelloWorld.....");
-	}
-
-}
