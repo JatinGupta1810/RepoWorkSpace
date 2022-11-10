4ebdd8e918bbfe0ff375778d2a2cc053ce18f983
JatinGupta1810 <jatingupta3556@gmail.com>
2022-11-10 13:35:27 +0530

    updated question_3 code

--- Assignment_1/Question_3.java
+++ Assignment_1/Question_3.java
@@ -6,6 +6,7 @@ public class Question_3 {
 		int n = 11;
 
 		for (int i = 0; i < n; i++) {
+			
 			for (int j = 0; j < n; j++) {
 				if (j == 0 || j == n - 1 || i == 0 || i == n - 1
 						|| (i <= n / 2 && (j <= (n / 2 - i) || j >= (n / 2 + i))))
@@ -13,6 +14,7 @@ public class Question_3 {
 				else
 					System.out.print(" ");
 			}
+			
 			System.out.println();
 		}
 	}
